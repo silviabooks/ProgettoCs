99bd13078591fb4b159fad3c0223ad805970679f
silvias94 <silviasottile94@gmail.com>
2017-11-01 11:35:15 +0100

    Aggiungere i file di progetto.

---
+++ ProgettoCs/ClientApp/app/components/posts-list/posts-list.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'posts-list',
+    templateUrl: './posts-list.component.html',
+    styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent {
+
+}
